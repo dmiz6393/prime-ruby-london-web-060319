3b19f23485f70f652a44cf33e590603774f25039
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-05 13:06:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
+
+
 def prime?(num)
-  return false if num < 2
-  (2...num).each do |factor|
-    if (num % factor).zero? 
-      return false
-    end
-  end
-  
-  true 
-end
+  return false if num<2 
+  (2...num-1).any? {|factor| num%factor==0}
+  return false 
+end 
+end 
+true
+end 
 
