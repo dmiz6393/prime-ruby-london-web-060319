5a5bdc631817ad8d2b7cfc9826dac9fbf705e6b6
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-05 13:02:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  (2...num).each do |factor|
+    if (num % factor).zero? 
+      return false
+      if num<2 
+        return false 
+end 
+end 
+return true 
+end 
+
